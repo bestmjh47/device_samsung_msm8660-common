8b8751d215317be94239afe64036a438e57d5155
Dan Cartier <nospamdan11@gmail.com>
2015-03-12 22:42:30 -0400

    Revert "msm8660: Restore power HAL"
    This reverts commit b0340144473b2d9b301573e13873b4ecb782c077.

--- msm8660.mk
+++ msm8660.mk
@@ -189,10 +189,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/test_radio_version.sh:/system/etc/test_radio_version.sh
 
-# Power HAL
-PRODUCT_PACKAGES += \
-    power.msm8660
-
 # USB
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
